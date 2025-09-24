2598d8029a8e221d0e96ebb7f025daa317dcbca3
jacob <jacob.kish@tc.tc>
2025-09-24 16:24:46 -0400

    Updates to newLogin and updateStory and default timeout on config

--- tests/newLogin.spec.ts
+++ tests/newLogin.spec.ts
@@ -1,12 +1,16 @@
+//To get around single sign on for other tests, this case is used to generate an auth (cookie) file called auth.json.
+//If other cases are blocked because a sign-on is required, run this case and sign in manually with the browser. Wait til the window closes and the cookies will be saved in ~/auth.json.
+
 import {test, expect, chromium} from '@playwright/test';
 
 test('navigate', async () =>{
-    test.setTimeout(6000000);
+    test.setTimeout(6000000); //Need to manually set timeout to give enough time to manually log in
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://qaflyerworksui.azurewebsites.net/",{waitUntil: 'networkidle'});
-    await page.waitForTimeout(35000);
-    await context.storageState({ path: 'auth.json' });
+    await page.waitForTimeout(35000); //35 seconds to enter email, password, and do 2FA
+
+    await context.storageState({ path: 'auth.json' }); //Save cookies to auth.json
     await browser.close();    
 });
--- tests/updateSaveStory.spec.ts
+++ tests/updateSaveStory.spec.ts
@@ -1,55 +1,66 @@
-import {test, expect, chromium} from '@playwright/test'
-//TC C5759
-test('navigate', async () =>{
-    test.setTimeout(6000000);
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext({storageState: 'auth.json'});
+//Simulates C5759 - Update save slot story
+import {test, expect, chromium, type Browser, type Page, type BrowserContext} from '@playwright/test'
 
-    const page = await context.newPage();
+let browser : Browser;
+let page: Page;
+let context: BrowserContext;
+
+
+
+test.describe('Change and reset value', ()=>{
+    test('navigate', async () =>{
+    //test.setTimeout(6000000); 
+
+    //Launch new Chrome browser with cookies from auth.json
+    browser = await chromium.launch({ headless: false });
+    context = await browser.newContext({storageState: 'auth.json'});
+
+    //Navigate to PromoBuilder QA
+    page = await context.newPage();
     await page.goto("https://qaflyerworksui.azurewebsites.net/",{waitUntil: 'networkidle'});
+
+
+    await page.getByRole('heading', { name: '2025-0325_271195' }).click(); //Click on deal D0325
+    await page.waitForLoadState('networkidle'); //Wait for all the $h!t to load
+    await page.getByText('Slot 2').first().click(); //Click on slot 2
     await page.waitForTimeout(1000);
 
-    await page.getByRole('heading', { name: '2025-0325_271195' }).click();
-    await page.waitForTimeout(15000);
-    await page.getByText('Slot 2').first().click();
-    await page.waitForTimeout(1000);
-    await page.getByText('SAVE UP TO  $130 ').click();
+    //Replace $130 save with $200
+    const row = page.locator('tr').locator('[data-cy="group-row-76-5136-8"]');
+    const saveStory = row.locator('[data-cy="save-story-value"]');
+    await saveStory.getByText('SAVE UP TO $130').first().click();
     await page.waitForTimeout(1000);
     const numBox = page.getByRole('textbox');
     await numBox.click();
     await numBox.fill('200');
     await page.waitForTimeout(1000);
     await page.getByRole('button', { name: 'Save' }).click();
+
+    //Confirm the text now displays $200
     const element = page.locator('[data-cy="heroSaveStory-value-2"]');
     await expect(element).toContainText("SAVE UP TO  $200 ");
 
 
-    await page.waitForTimeout(10000); 
+    //await page.waitForTimeout(10000); //10 sec wait at the end for debug purposes only
 });
 
-test.afterAll('Restore original state', async ()=>{
-    test.setTimeout(70000);
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext({storageState: 'auth.json'});
+    test.afterEach('Restore original state', async ()=>{
+        //await page.getByRole('heading', { name: '2025-0325_271195' }).click();
 
-    const page = await context.newPage();
-    await page.goto("https://qaflyerworksui.azurewebsites.net/",{waitUntil: 'networkidle'});
-    await page.waitForTimeout(1000);
+        //Cleanup section for consistency : slot 2 item 1 should always return to save story "Save up to $130"
+        const row = page.locator('tr').locator('[data-cy="group-row-76-5136-8"]');
+        const saveStory = row.locator('[data-cy="save-story-value"]');
+        await saveStory.getByText('SAVE UP TO $200').first().click();
+        //await page.locator('div.MuiStack-root.css-43ziog').getByText('SAVE UP TO  $200 ').click();
+        await page.waitForTimeout(1000);
+        const numBox = page.getByRole('textbox');
+        await numBox.click();
+        await numBox.fill('130');
+        await page.waitForTimeout(1000);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(saveStory).toContainText("SAVE UP TO  $130 ");
+        await browser.close();
 
-    await page.getByRole('heading', { name: '2025-0325_271195' }).click();
-    await page.waitForTimeout(15000);
-    await page.getByText('Slot 2').first().click();
-    await page.waitForTimeout(1000);
-    await page.getByText('SAVE UP TO  $200 ').click();
-    await page.waitForTimeout(1000);
-    const numBox = page.getByRole('textbox');
-    await numBox.click();
-    await numBox.fill('130');
-    await page.waitForTimeout(1000);
-    await page.getByRole('button', { name: 'Save' }).click();
-    const element = page.locator('[data-cy="heroSaveStory-value-2"]');
-    await expect(element).toContainText("SAVE UP TO  $130 ");
-    await browser.close();
-
-
+    });
 });
+
